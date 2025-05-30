d669cb8060741e45f3c8ca7a3a2130e56c3eeafe
bensky103 <guybensky010@gmail.com>
2025-05-30 11:19:33 +0300

    fixing

--- stock_data_fetching/main.py
+++ stock_data_fetching/main.py
@@ -7,12 +7,12 @@ from typing import Dict, Any, Optional
 import uvicorn
 import pandas as pd
 
-from fetch_price_data import fetch_price_data
-from calculate_indicators import add_technical_indicators
-from calculate_volume_features import calculate_volume_features
-from fetch_fundamentals import fetch_fundamentals
-from config import settings
-from logger import logger
+from .fetch_price_data import fetch_price_data
+from .calculate_indicators import add_technical_indicators
+from .calculate_volume_features import calculate_volume_features
+from .fetch_fundamentals import fetch_fundamentals
+from .config import settings
+from .logger import logger
 
 app = FastAPI(
     title="Stock Data Fetching Service",
